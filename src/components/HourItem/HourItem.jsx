bfed5bfa17fe9d09c1bf3a28c47192ccb9a21676
v-kulikov-dev <23vitas93@mail.ru>
2020-12-10 16:47:48 +0300

    Add hour component

---
+++ src/components/HourItem/HourItem.jsx
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+const HourItem = ({ hour, api }) => {
+  const time = moment(hour.dt * 1000).format("hh a");
+  const image = `${api.image}${hour.weather[0].icon}@2x.png`;
+
+  return (
+    <div className="forecast-item sul-box-raised-1 with-hover" key={hour.id}>
+      <div>{time}</div>
+      <div className="item-temperature">{Math.round(hour.temp)}°C</div>
+      <div className="weather-logo">
+        <img src={image} alt="weather-logo" />
+      </div>
+      <div>{hour.weather[0].main}</div>
+    </div>
+  );
+};
+
+export default HourItem;
--- src/components/HourlyForecast/HourlyForecast.js
+++ src/components/HourlyForecast/HourlyForecast.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import moment from "moment";
+import HourItem from "../HourItem";
 
 const HourlyForecast = ({ coord, api }) => {
   const [hourlyWeather, setHourlyWeather] = useState([]);
@@ -12,26 +12,12 @@ const HourlyForecast = ({ coord, api }) => {
       .then((result) => {
         setHourlyWeather(result.hourly.slice(1, 4));
       });
-  }, [coord]);
+  }, [coord, api]);
 
   return (
     <div className="forecast-container sul-box-inset-1">
       {hourlyWeather.map((hour) => {
-        const time = moment(hour.dt * 1000).format("hh a");
-        const image = `${api.image}${hour.weather[0].icon}@2x.png`;
-        return (
-          <div
-            className="forecast-item sul-box-raised-1 with-hover"
-            key={hour.id}
-          >
-            <div>{time}</div>
-            <div className="item-temperature">{Math.round(hour.temp)}°C</div>
-            <div className="weather-logo">
-              <img src={image} alt="weather-logo" />
-            </div>
-            <div>{hour.weather[0].main}</div>
-          </div>
-        );
+        return <HourItem hour={hour} api={api} />;
       })}
     </div>
   );
