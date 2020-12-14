047ffb746a84b8499d7a50d537e7265d9256dd10
v-kulikov-dev <23vitas93@mail.ru>
2020-12-14 17:54:45 +0300

    Fixed issue with keys

--- src/components/HourlyDashboard/HourlyDashboard.jsx
+++ src/components/HourlyDashboard/HourlyDashboard.jsx
@@ -16,13 +16,13 @@ const HourlyDashboard = ({ history, weather }) => {
       <div className="sul-box-raised-3 hourly-dashboard-wrapper">
         <h1>48 Hour Forecast</h1>
         <button className="unit" type="button" onClick={goBack}>
-          <i className="icon ion-md-arrow-back"></i>
+          <i className="icon ion-md-arrow-back" />
         </button>
       </div>
       <div className="sul-box-raised-3 hour-list">
         {weather &&
           weather.map((el) => {
-            return <HourlyDashboardItem hourWeather={el} />;
+            return <HourlyDashboardItem hourWeather={el} key={el.dt} />;
           })}
       </div>
     </>
