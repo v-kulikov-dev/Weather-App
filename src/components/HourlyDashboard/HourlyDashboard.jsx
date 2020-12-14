26ac23392496ff04ec9386f6ffac9c9d8c94d6c2
v-kulikov-dev <23vitas93@mail.ru>
2020-12-14 16:36:39 +0300

    Added 'back to search' button

--- src/components/HourlyDashboard/HourlyDashboard.jsx
+++ src/components/HourlyDashboard/HourlyDashboard.jsx
@@ -7,10 +7,17 @@ const HourlyDashboard = ({ history, weather }) => {
     history.push("/");
   }
 
+  const goBack = () => {
+    history.goBack();
+  };
+
   return (
     <>
       <div className="sul-box-raised-3 hourly-dashboard-wrapper">
         <h1>48 Hour Forecast</h1>
+        <button className="unit" type="button" onClick={goBack}>
+          <i className="icon ion-md-arrow-back"></i>
+        </button>
       </div>
       <div className="sul-box-raised-3 hour-list">
         {weather &&
