5cb3301a27773605b9b8c1e162b1fee20ce234a5
v-kulikov-dev <23vitas93@mail.ru>
2021-02-03 19:05:41 +0300

    Remove servoce worker

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './service-worker';
 
 import './index.scss';
 import 'soft-ui-library/dist/css/neumorphism-ui.css';
-
-serviceWorker.register();
 
 ReactDOM.render(
   <BrowserRouter>
