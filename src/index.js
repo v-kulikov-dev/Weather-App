2c3527587d20160694e376637844dab139c66635
v-kulikov-dev <23vitas93@mail.ru>
2021-02-05 10:21:20 +0300

    Add && Setup service-worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './index.scss';
 import 'soft-ui-library/dist/css/neumorphism-ui.css';
@@ -14,4 +15,10 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 reportWebVitals();
